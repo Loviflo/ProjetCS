1b096b6f72dfb901d128992da41df4a09d66c555
Vivian <vivian.fr@free.fr>
2021-02-12 16:12:52 +0100

    Limit, Score and Map

--- PHP/games.php
+++ PHP/games.php
@@ -4,13 +4,19 @@
     ini_set('display_errors', 1);
 
     $offset = isset($_GET['offset']) && !empty($_GET['offset']) ? intval($_GET['offset']) : 0;
-    $limit = isset($_GET['limit']) && !empty($_GET['limit']) ? intval($_GET['limit']) : 20;
+    $limit = isset($_GET['limit']) && !empty($_GET['limit']) ? intval($_GET['limit']) : 100;
     $db = getDatabaseConnection();
     $where = [];
     $params = [];
-    if(isset($_GET['Score'])) {
-        array_push($where, 'Score LIKE ?');
-        $params[] = "%". $_GET['Score'] . "%"; // eq array_push
+    if(!isset($_GET['Score'])){$_GET['Score'] = '';}
+    if(!empty($_GET['Score'])) {
+        $where[] = 'Score = ?';
+        $params[] = $_GET['Score']; // eq array_push
+    }
+    if(!isset($_GET['Carte'])){$_GET['Carte'] = '';}
+    if(!empty($_GET['Carte'])) {
+        $where[] = 'Carte LIKE ?';
+        $params[] = "%". $_GET['Carte'] . "%"; // eq array_push
     }
     $sql = 'SELECT ID, Date, RankY, RankV, Score, Carte, Résultat, RankE1, RankE2, Info FROM wingman';
     if(count($where) > 0) {
@@ -26,30 +32,48 @@
             $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
         }
     }
+    $counter = 0; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>        <script src="chart.js"></script>    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="icon" type="image/png" sizes="16x16" href="http://viviansrv.ddns.net/Images/logoCSGO.png">
     <title>Games</title>
 </head>
 <body>
+<?php include "../utils/header.php"; ?>
     <form method="GET" action="games.php">
         <div class="form-group">
         <label class="form-label">Limit</label>
-        <input type="text" class="form-control" name="limit" id="">
+        <?php echo '<input type="text" class="form-control" name="limit" value="'. $limit .'" id="" placeholder="Limite...">'; ?>
         <label class="form-label">Score</label>
-        <input type="text" class="form-control" name="Score" id="">
+        <?php echo '<input type="text" class="form-control" name="Score" value="'. $_GET["Score"] .'" id="" placeholder="Score...">'; ?>
+        <label for="">Carte</label>
+        <select class="form-control" name="Carte">
+            <option value="">Choisi une carte</option>
+            <option <?php echo $_GET['Carte'] == 'Nuke' ? 'selected' : ''; ?>>Nuke</option>
+            <option <?php echo $_GET['Carte'] == 'Cobblestone' ? 'selected' : ''; ?>>Cobblestone</option>
+            <option <?php echo $_GET['Carte'] == 'Lake' ? 'selected' : ''; ?>>Lake</option>
+            <option <?php echo $_GET['Carte'] == 'Inferno' ? 'selected' : ''; ?>>Inferno</option>
+            <option <?php echo $_GET['Carte'] == 'Train' ? 'selected' : ''; ?>>Train</option>
+            <option <?php echo $_GET['Carte'] == 'Overpass' ? 'selected' : ''; ?>>Overpass</option>
+            <option <?php echo $_GET['Carte'] == 'Rialto' ? 'selected' : ''; ?>>Rialto</option>
+            <option <?php echo $_GET['Carte'] == 'Vertigo' ? 'selected' : ''; ?>>Vertigo</option>
+            <option <?php echo $_GET['Carte'] == 'Elysion' ? 'selected' : ''; ?>>Elysion</option>
+            <option <?php echo $_GET['Carte'] == 'Guard' ? 'selected' : ''; ?>>Guard</option>
+            <option <?php echo $_GET['Carte'] == 'Shortdust' ? 'selected' : ''; ?>>Shortdust</option>
+        </select>
         </div>
-        <button type="submit">Afficher</button>
+        <button type="submit" class="btn btn-primary">Afficher</button>
     </form>
-    <table class="table">
-        <thead>
+    <table class="table table-striped">
+        <thead class="thead-dark">
             <tr>
                 <th>Date</th>
                 <th>Rang Yanic</th>
@@ -75,7 +99,11 @@
                 <td><?php echo $value['RankE2']; ?></td>
                 <td><?php echo $value['Info']; ?></td>
             </tr>
-        <?php } ?>
+        <?php 
+            $counter++; 
+            } 
+            echo "<h3>Nombre de lignes : " . $counter . "</h3>"
+        ?>
         </tbody>
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
---
+++ utils/header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="http://viviansrv.ddns.net ">
+            <img src="http://viviansrv.ddns.net/Images/logoCSGO.png" alt="" width="30" height="24" class="d-inline-block align-top">
+            CS GO
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="navbar-brand" href="http://viviansrv.ddns.net">Accueil</a>
+                <a class="nav-link active" aria-current="page" href="#">Formulaire</a>
+                <a class="nav-link" href="http://viviansrv.ddns.net/PHP/games.php">Statistiques</a>
+                <a class="nav-link" href="#">Profil</a>
+            </div>
+        </div>
+    </div>
+</nav>
